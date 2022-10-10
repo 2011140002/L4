0e47bf201af2c72f13ee060ae2d928002fe39dcb
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-10 14:41:27 +0000

    Completed 200 OK in 35ms (Views: 34.8ms | ActiveRecord: 0.0ms)

--- CS2/config/routes.rb
+++ CS2/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'accounts/main'
   post 'accounts/login'
   get 'accounts/error'
-  post 'accounts' ,to: 'accounts#login'
+  #post 'accounts' ,to: 'accounts#login'
   delete 'accounts' ,to: 'accounts#logout'
   
 end
