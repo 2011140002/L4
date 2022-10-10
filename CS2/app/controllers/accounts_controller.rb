14fd7af1d09f5f0ab685b6484a0b41766b7c1ba0
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-10 13:11:11 +0000

    ActionController::RoutingError (No route matches [GET] "/accounts/login"):

--- CS2/app/controllers/accounts_controller.rb
+++ CS2/app/controllers/accounts_controller.rb
@@ -11,12 +11,12 @@ class AccountsController < ApplicationController
     uid = params[:uid]
     pass = params[:pass]
     logger.debug((Account.find_by(uid: uid, pass: pass)).inspect)
-    if Account.find_by(uid: uid, pass: pass)
+    if Account.find_by(uid: uid, pass: pass) == nil
       session[:login_uid] = uid
       redirect_to 'main'
     else
       session[:login_uid] = nil
-      # render 'error'
+      render 'error'
     end
   
   end
