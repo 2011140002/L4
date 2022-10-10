e9a15e341abc76c1b46bcac1a4128456ad83a7bc
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-10 14:49:13 +0000

    Completed 200 OK in 40ms (Views: 39.1ms | ActiveRecord: 0.0ms)

--- CS2/app/controllers/accounts_controller.rb
+++ CS2/app/controllers/accounts_controller.rb
@@ -8,33 +8,33 @@ class AccountsController < ApplicationController
   end
   
   def login
-    account = Account.find_by(uid: params[:uid])
-    if account && account[:pass] == params[:pass]
-      session[:login_uid] = params[:uid]
-      redirect_to root_path
-    else
-      render'error'
-    end
-    # uid = params[:uid]
-    # pass = params[:pass]
-    # logger.debug(uid)
-    # logger.debug(pass)
-    # logger.debug(Account.find_by(uid: uid, pass: pass))
-    # if Account.find_by(uid: params[:uid], pass: params[:pass]) == nil
-    #   session[:login_uid] = nil
-    #   render 'error'
-      
+    # account = Account.find_by(uid: params[:uid])
+    # if account && account[:pass] == params[:pass]
+    #   session[:login_uid] = params[:uid]
+    #   redirect_to root_path
     # else
-    #   session[:login_uid] = uid
-    #   redirect_to 'main'
-      
+    #   render'error'
     # end
+    uid = params[:uid]
+    pass = params[:pass]
+    logger.debug(uid)
+    logger.debug(pass)
+    logger.debug(Account.find_by(uid: uid, pass: pass))
+    if Account.find_by(uid: params[:uid], pass: params[:pass]) == nil
+      session[:login_uid] = nil
+      render 'error'
+      
+    else
+      session[:login_uid] = uid
+      redirect_to 'main'
+      
+    end
   
   end
   
   def logout
     session[:login_uid] = nil
-    redirect_to 'main'
+    redirect_to root_path
   end
   
   def error
